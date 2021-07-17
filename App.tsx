cfe59859ff3e2f78cbf8f6d6be6f40b14b99c2d9
lauren-west <lwest@hmc.edu>
2021-07-17 12:30:18 -0700

    display meal description in app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import MealDescription from "./components/meal/meal-description";
 import MealStarRating from "./components/meal/meal-star-ratings";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <MealDescription description="Hamburger contain meat yum!"/>
       <MealStarRating />
       <StatusBar style="auto" />
     </View>
